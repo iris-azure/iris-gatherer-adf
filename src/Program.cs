49559c335b5b7fb1f51808a87bda8b12dca60d8c
Biswajit Dhar <34776555+bdhar1@users.noreply.github.com>
2021-10-25 08:26:07 +0000

    Added project scaffolding

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IrisGathererADF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
